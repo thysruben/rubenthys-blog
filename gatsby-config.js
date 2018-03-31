79ea723bd68ca8a0b50e6db001a0b99cd96c798c
Ruben Thys <ruben.thys@student.odisee.be>
2018-03-31 16:56:20 +0100

    feat(): change title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Blog',
+    title: 'Ruben\'s blog',
     author: 'Kyle Mathews',
     description: 'A starter blog demonstrating what Gatsby can do.',
     siteUrl: 'https://gatsbyjs.github.io/gatsby-starter-blog/',
